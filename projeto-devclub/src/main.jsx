039a66ac3e4090229e5043705ad767c2543ba7a2
andresilva05 <andresilva9657@gmail.com>
2024-07-19 13:40:39 -0300

    adição de pasta e alteração na estilização

--- projeto-devclub/src/main.jsx
+++ projeto-devclub/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Atualizado para 'react-dom/client'
-import App from './App';
-import './index.css';
+import Home from './pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Cria a raiz do aplicativo
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
